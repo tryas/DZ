98738f3f8de48db0690fd51306123c1f644a39d0
tryas <dradeev@gmail.com>
2021-10-05 14:44:29 +0300

    task DZ_3_6 на_работе

--- DZ_3/DZ_3_6.py
+++ DZ_3/DZ_3_6.py
@@ -1,3 +1,9 @@
 # Реализовать функцию int_func(), принимающую слова из маленьких
 # латинских букв и возвращающую их же, но с прописной первой буквой.
 # Например, print(int_func(‘text’)) -> Text.
+
+def int_func(new_string):
+    new_string = new_string.title()
+    return(new_string)
+
+print(int_func('привет как дела'))
