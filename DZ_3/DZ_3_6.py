5d82d454afa5e7e944f6048875d81d44e1fa994f
tryas <dradeev@gmail.com>
2021-10-05 16:51:24 +0300

    task DZ_3_7 на_работе

--- DZ_3/DZ_3_6.py
+++ DZ_3/DZ_3_6.py
@@ -2,6 +2,8 @@
 # латинских букв и возвращающую их же, но с прописной первой буквой.
 # Например, print(int_func(‘text’)) -> Text.
 
+#Честно говоря. не очень понятно...))) Слишком легко
+
 def int_func(new_string):
     new_string = new_string.title()
     return(new_string)
